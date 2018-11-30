a63208b01fed536794ec6593aaa214c20befbc63
tabay <hamide.bay@stud.hslu.ch>
2018-11-30 15:03:13 +0100

    css Anpassungen

--- Reddit/js/controller.js
+++ Reddit/js/controller.js
@@ -2,19 +2,26 @@ $(document).ready(function(){
 
 function veriAl(){
     let benutzerName = $('#Benutzername').val();
+    console.log(benutzerName);
     let passWord = $('#Password').val();
     let newTopic = $('#topic-name').val();
     let title = $('#post-Title').val();
     let message = $('#post-Message').val();
     let comment = $('#post-Comment').val();
 
-    createUser(benutzerName, passWord);
+    getUser(benutzerName, passWord);
 
     return {benutzerName, passWord, newTopic, title, message, comment};
 }
 
-$('#newUserBtn').on('click', function(){
 
+
+
+
+$('#newUserBtn').on('click', function(){
+let benutzer = veriAl().benutzerName;
+let passWord = veriAl().passWord;
+createUser(benutzer, passWord);
 });
 
 $('#newTopicBtn').on('click', function(){
--- Reddit/js/model.js
+++ Reddit/js/model.js
@@ -1,3 +1,25 @@
-function createUser(username, password){
-    
+
+function createUser(userName, passWord)
+{
+    //let user = userlist[0];
+    let userString = localStorage.getItem(userName);
+    let OneUser = JSON.parse(userString);
+           
+    if(OneUser){
+        OneUser.push(userString);
+    }else{
+        OneUser = [user]; 
+    }
+
+    localStorage.setItem(user, JSON.stringify(OneUser));
+
+  
+}
+
+
+function getUser(benutzerName, passWord){
+    let users = [];
+    users.push({"benutzerName": $(benutzerName) , "passWord": $(passWord)});
+    console.log(users);
+    return users;
 }

4692231532c79284538178f940fbb5ddd82dd2af
tabay <hamide.bay@stud.hslu.ch>
2019-01-20 23:17:40 +0100

    BatteryFactory.fillBatteryColor() eingefügt

--- Battery/js/BatteryFactory.js
+++ Battery/js/BatteryFactory.js
@@ -8,9 +8,13 @@
 
 class BatteryFactory {
     constructor() {
-         this.maxCapacity = 100;
-         this.currentEnergy = 80;
-         this.emptyBattery = 0;
+        this.maxCapacity = 100;
+        this.currentEnergy = 80;
+        this.emptyBattery = 0;
+        this.width = this.currentEnergy;
+        this.height = 50;
+        this.colorREd = "red";
+        this.colorGreen = "green";
     }
 
     getCurrentEnergy() {
@@ -20,7 +24,7 @@ class BatteryFactory {
     loadBattery() {
         let loading = setInterval(() => {
             this.currentEnergy += 5;
-            if(this.currentEnergy >= this.maxCapacity){
+            if (this.currentEnergy >= this.maxCapacity) {
                 clearInterval(loading);
                 alert('Battery is full');
                 this.reduceEnergy();
@@ -30,14 +34,29 @@ class BatteryFactory {
 
     reduceEnergy() {
         let reducing = setInterval(() => {
+            this.fill();
             this.currentEnergy -= 5;
-            if(this.currentEnergy === this.emptyBattery){
+            if (this.currentEnergy === this.emptyBattery) {
                 clearInterval(reducing);
                 alert('Empty Battery');
                 alert(this.currentEnergy);
             }
         }, 1000);
 
+    }
+
+    fill(){
+        let color = `<div class="battery-color" style= "width: ${this.width}px; height: ${this.height}; background-color: ${this.colorGreen};"  ></div>`;
+        $('div').html(color);
+    }
+
+    fillBatteryColor() {
+        let batteryColor = `<section><div class="battery-color" style= "width: ${this.width}px; height: ${this.height}; background-color: ${this.colorGreen};"  ></div></section>`;
+        if(this.width > 20){return wi;}
+        else{
+            batteryColor = `<section><div class="battery-color" style= "width: ${this.width}px; height: ${this.height}; background-color: ${this.colorREd};"  ></div></section>`;
+        }
+        $('section div').html(batteryColor);
     }
 
 
